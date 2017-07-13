c06f5ab8a4c3cb5bcc18a1eb020ec80accaa4d11
ManicSec <>
2017-07-13 23:45:06 +0100

    Default

--- r0mod/r0mod.c
+++ r0mod/r0mod.c
@@ -1,15 +1,49 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/dirent.h>
-#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/unistd.h>
+#include <asm/current.h>
+#include <linux/sched.h>
 #include <linux/syscalls.h>
+#include <asm/system.h>
 
 #include <r0mod/global.h>
 
+#define START_MEM   0xc0000000
+#define END_MEM     0xd0000000
+
+unsigned long *syscall_table;
+
+unsigned long **find()
+{
+    unsigned long **sctable;
+    unsigned long int i = START_MEM;
+
+    while(i < END_MEM)
+    {
+        sctable = (unsigned long **)i;
+
+        if(sctable[__NR_close] == (unsigned long *) sys_close)
+            return &sctable[0];
+
+        i += sizeof(void*);
+    }
+
+    return NULL;
+}
+
 static int __init r0mod_init(void)
 {
-    fm_alert("%s\n", "R0Mod: Initialized.");
+    printk("Module starting...\n");
+
+    syscall_table = (unsigned long*)find();
+
+    if(syscall_table != NULL)
+        printk("Syscall table found at %x\n", (unsigned )syscall_table);
+    else
+        printk("Syscall table not found!\n");
 
     return 0;
 }
@@ -17,7 +51,7 @@ static int __init r0mod_init(void)
 
 static void __exit r0mod_exit(void)
 {
-    fm_alert("%s\n", "R0Mod: Uninitialized.");
+    printk("Module ending...\n");
 
     return;
 }
