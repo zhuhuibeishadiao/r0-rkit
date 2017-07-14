ab902abd76beb2bc765dc5f463ec6e81b9a319da
ManicSec <>
2017-07-14 06:22:46 +0100

    Default

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all:
+	cd r0bin && make
 	cd r0mod && make
 
 clean:
+	cd r0bin && make clean
 	cd r0mod && make clean
---
+++ r0bin/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+    setreuid (1000, 1000);
+    system("/bin/sh");
+    return 0;
+}
